6d91b3a739c5b003495204d437b9f90dabfa4fb7
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-06-02 11:06:21 +0530

    dynamic & dart support added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+
 
 function App() {
+  const editorRef = useRef(null);
+  const monaco = useMonaco();
+
+  // Define a custom theme (optional)
+  useEffect(() => {
+    if (monaco) {
+      monaco.editor.defineTheme("custom-dark", {
+        base: "vs-dark",
+        inherit: true,
+        rules: [
+          { token: "comment", foreground: "7f848e", fontStyle: "italic" },
+          { token: "keyword", foreground: "c586c0" },
+          { token: "number", foreground: "b5cea8" },
+        ],
+        colors: {
+          "editor.background": "#1e1e1e",
+          "editorLineNumber.foreground": "#858585",
+        },
+      });
+      monaco.languages.register({ id: "dart" });
+
+monaco.languages.setMonarchTokensProvider("dart", {
+  tokenizer: {
+    root: [
+      [/[a-z_$][\w$]*/, {
+        cases: {
+          '@keywords': 'keyword',
+          '@default': 'identifier'
+        }
+      }],
+      [/[A-Z][\w\$]*/, 'type.identifier'],
+      { include: '@whitespace' },
+      [/[{}()\[\]]/, '@brackets'],
+      [/[<>](?!@symbols)/, '@brackets'],
+      [/@symbols/, {
+        cases: {
+          '@operators': 'operator',
+          '@default': ''
+        }
+      }],
+      [/\d+/, 'number'],
+      [/[;,.]/, 'delimiter'],
+      [/"([^"\\]|\\.)*$/, 'string.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }]
+    ],
+    string: [
+      [/[^\\"]+/, 'string'],
+      [/\\./, 'string.escape.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+    ],
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/\/\*\*(?!\/)/, 'comment.doc', '@jsdoc'],
+      [/\/\*/, 'comment', '@comment'],
+      [/\/\/.*$/, 'comment']
+    ],
+    comment: [
+      [/[^\/*]+/, 'comment'],
+      [/\*\//, 'comment', '@pop'],
+      [/[\/*]/, 'comment']
+    ],
+    jsdoc: [
+      [/[^\/*]+/, 'comment.doc'],
+      [/\*\//, 'comment.doc', '@pop'],
+      [/[\/*]/, 'comment.doc']
+    ]
+  },
+  keywords: [
+    "abstract", "else", "import", "super",
+    "as", "enum", "in", "switch",
+    "assert", "export", "interface", "sync",
+    "async", "extends", "is", "this",
+    "await", "extension", "late", "throw",
+    "break", "external", "library", "true",
+    "case", "factory", "mixin", "try",
+    "catch", "false", "new", "typedef",
+    "class", "final", "null", "var",
+    "const", "finally", "on", "void",
+    "continue", "for", "operator", "while",
+    "covariant", "Function", "part", "with",
+    "default", "get", "required", "yield",
+    "deferred", "hide", "rethrow",
+    "do", "if", "return",
+    "dynamic", "implements", "set"
+  ],
+  operators: [
+    '=', '>', '<', '!', '~', '?', '??',
+    '==', '<=', '>=', '!=', '&&', '||', '++', '--',
+    '+', '-', '*', '/', '&', '|', '^', '%', '<<',
+    '>>', '>>>', '+=', '-=', '*=', '/=', '&=',
+    '|=', '^=', '%=', '<<=', '>>=', '>>>='
+  ],
+  symbols: /[=><!~?:&|+\-*\/\^%]+/
+});
+
+    }
+  }, [monaco]);
+
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Editor
+        height="100%"
+        theme="custom-dark"
+        defaultLanguage="dart"
+        defaultValue={`void main() {\n  print('Hello from Monaco!');\n}`}
+        onMount={handleEditorDidMount}
+      />
     </div>
   );
 }
